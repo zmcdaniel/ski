48ca1f4310914737783aa881130fbbb5e14a69d1
zmcdaniel <zmcdaniel@gmail.com>
2018-11-16 18:02:41 -0800

    Added AJAX function to get snow report data specifically for Winter Park. JSON data is returning

--- ski.js
+++ ski.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     console.log("Ready to go!");
+    getData();
 });
+
+/**
+ * Fetching snow report using AJAX
+ */
+function getData() {
+    let uri = "https://snowreporting.herokuapp.com/feed";
+
+    $.ajax({
+        url: uri,
+        method: "GET",
+        crossDomain: true,
+        data: {
+            resortId: 5
+        }
+    }).done(function(responseData){
+        console.log(responseData);
+    }).fail(function(error) {
+        console.log(error);
+    });
+}
